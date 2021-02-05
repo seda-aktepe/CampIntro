217eb74f3e478db6dc97ac41cf6a8608b13b832b
Seda Aktepe <ruzgartufan@gmail.com>
2021-02-06 00:19:41 +0300

    ödev4.5

---
+++ Odev4_5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Odev4_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, int> myDictionary = new MyDictionary<string, int>();
+            myDictionary.Add("ordu", 52);
+            myDictionary.Add("rize", 53);
+
+
+            
+        }
+    }
+}
